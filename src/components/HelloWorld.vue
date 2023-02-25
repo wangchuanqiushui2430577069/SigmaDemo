97d33c7babff4f9262ba9ee4359e9d61e3f02dd9
忘川秋水 <2430577069@qq.com>
2023-02-25 19:09:51 +0800

    version 1.x

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,22 +28,22 @@ export default defineComponent({
     let transformData = () => {
       // HACK:转换官方dataset.json数据，为finalData.json数据 (Change official data to finalData.json)
       // tag=>对应图片（'tag' Mapping to the image's url）
-      let tagImageMap: Map<String, String> = new Map<String, String>();
+      let tagImageMap: Map<String, String> = new Map<String, String>()
       data.tags.forEach(tag => tagImageMap.set(tag.key, tag.image))
 
       // cluster=>对应颜色（'cluster' Mapping to the corresponding color）
-      let clusterColorMap: Map<String, String> = new Map<String, String>();
+      let clusterColorMap: Map<String, String> = new Map<String, String>()
       data.clusters.forEach(cluster => clusterColorMap.set(cluster.key, cluster.color))
       // cluster=>对应集群名（'cluster' Mapping to the cluster name）
-      let clusterNameMap: Map<String, String> = new Map<String, String>();
+      let clusterNameMap: Map<String, String> = new Map<String, String>()
       data.clusters.forEach(cluster => clusterNameMap.set(cluster.key, cluster.clusterLabel))
       // 计算node大小(Calculate the node size by its Relational number)
-      let sizeMap: Map<String, number> = new Map<String, number>();
+      let sizeMap: Map<String, number> = new Map<String, number>()
 
       // 构建节点(Build node)
       data.nodes.forEach(node => {
         // build sizeMap
-        sizeMap.set(node.key, 1);
+        sizeMap.set(node.key, 1)
       })
       // 构建边(Build edge)
       let edgeIndex = 1
@@ -64,20 +64,20 @@ export default defineComponent({
             'score': node.score, 'x': node.x, 'y': node.y, color: clusterColorMap.get(node.cluster), 'clusterLabel': clusterNameMap.get(node.cluster)
           }
         }
-        return nodeTemp;
+        return nodeTemp
       })
       // 图所需结构(Required structure)
       let needData: NeedData = { edges: edges, nodes: nodes }
-      console.log('data', data);
+      console.log('data', data)
       console.log('needData', needData)
       // init tagsOptions
-      const tempTagsOptions: any[] = [];
+      const tempTagsOptions: any[] = []
       for (let name of tagImageMap.keys()) {
         let temp = { value: name, label: name }
         tempTagsOptions.push(temp)
       }
       // init clusterOptions
-      const tempClusterOptions: any[] = [];
+      const tempClusterOptions: any[] = []
       for (let name of clusterNameMap.values()) {
         let temp = { value: name, label: name }
         tempClusterOptions.push(temp)
@@ -85,19 +85,19 @@ export default defineComponent({
       return { data: needData, cluster: tempClusterOptions, tags: tempTagsOptions };
     }
     let init = (needData: NeedData) => {
-      const container = document.getElementById("card") as HTMLElement;
+      const container = document.getElementById("card") as HTMLElement
       // 初始化简单图(init graph)
-      const graph = new Graph();
+      const graph = new Graph()
       circlepack.assign(graph, {
         hierarchyAttributes: ['cluster'],
-      });
+      })
       // 布局(init layout)
       const sensibleSettings = forceAtlas2.inferSettings(graph);
       const fa2Layout = new FA2Layout(graph, {
         settings: sensibleSettings,
-      });
-      fa2Layout.start();
-      graph.import(needData);
+      })
+      fa2Layout.start()
+      graph.import(needData)
 
       //创建sigma (new a sigma class)
       const renderer = new Sigma(graph, container, {
@@ -106,64 +106,66 @@ export default defineComponent({
           image: getNodeProgramImage()
         },
         renderEdgeLabels: false, // 不显示边的label
-      });
+      })
       renderer.getCamera().setState({
         angle: 1,
-      });
+      })
+
       // HACK:  实现悬停节点显示领域(Realize the display field of hovering nodes)
-
-
-      let state: State = {};
+      let state: State = {}
       let setHoveredNode = (node?: string) => {
         if (node) {
-          state.hoveredNode = node;
-          state.hoveredNeighbors = new Set(graph.neighbors(node));
+          state.hoveredNode = node
+          state.hoveredNeighbors = new Set(graph.neighbors(node))
         } else {
-          state.hoveredNode = undefined;
-          state.hoveredNeighbors = undefined;
+          state.hoveredNode = undefined
+          state.hoveredNeighbors = undefined
         }
         // Refresh rendering:
-        renderer.refresh();
+        renderer.refresh()
       }
       // Bind graph interactions:
       renderer.on("enterNode", ({ node }) => {
-        setHoveredNode(node);
+        setHoveredNode(node)
       });
       renderer.on("leaveNode", () => {
-        setHoveredNode(undefined);
+        setHoveredNode(undefined)
       });
 
       renderer.setSetting("nodeReducer", (node, data) => {
-        const res: Partial<NodeDisplayData> = { ...data };
-
-        if (state.hoveredNeighbors && !state.hoveredNeighbors.has(node) && state.hoveredNode !== node) {
+        const res: Partial<NodeDisplayData> = { ...data }
+        const attributes = graph.getNodeAttributes(node)
+        if (attributes && !(tagsValue.value.includes(attributes.tag) && clusterValue.value.includes(attributes.clusterLabel))) {
           res.hidden = true;
-          // res.label = "";
-          // res.color = "#f6f6f6";
-          return res;
+        }
+        if (state.hoveredNeighbors && !state.hoveredNeighbors.has(node) && state.hoveredNode !== node) {
+          res.hidden = true
+          // res.label = ""
+          // res.color = "#f6f6f6"
+          return res
         }
         // 悬停(hovering time)
         if (state.hoveredNode === node) {
-          res.highlighted = true;
+          res.highlighted = true
         }
-        return res;
+        return res
       });
 
       // Render edges accordingly to the internal state:
       renderer.setSetting("edgeReducer", (edge, data) => {
-        const res: Partial<EdgeDisplayData> = { ...data };
+        const res: Partial<EdgeDisplayData> = { ...data }
         // 隐藏其他关系线(hide other lines and nodes)
         if (state.hoveredNode && !graph.hasExtremity(edge, state.hoveredNode)) {
-          res.hidden = true;
-          return res;
+          res.hidden = true
+          return res
         }
         if (state.hoveredNode && graph.hasExtremity(edge, state.hoveredNode)) {
           const attributes = graph.getNodeAttributes(state.hoveredNode)
-          res.color = attributes.color?.toString();
+          res.color = attributes.color?.toString()
           res.size = 3
         }
-        return res;
-      });
+        return res
+      })
     }
 
     let changeTagsOptions = (options) => {
@@ -175,17 +177,17 @@ export default defineComponent({
     // 得到所需数据（get data that you need）
     let result = transformData()
 
-
+    // 拓扑图渲染数据（Topology map rendering data）
     const finalData = ref(result.data)
-    const tagsOptions = ref(result.tags);
-    const tagsValue = ref([])
-    const clusterOptions = ref(result.cluster);
-    const clusterValue = ref([])
+    // 表单数据（form data）
+    const tagsOptions = ref(result.tags)
+    const clusterOptions = ref(result.cluster)
+    // 默认全选（Select all by default）
+    const tagsValue = ref(result.tags.map(tag => tag.value))
+    const clusterValue = ref(result.cluster.map(cluster => cluster.value))
 
     onMounted(() => {
       init(finalData.value)
-      console.log('tagsValue', tagsValue)
-      console.log('clusterValue', clusterValue)
     })
 
     return {
